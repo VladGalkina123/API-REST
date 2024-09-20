21e54f3d99a5368cfc283cbe8fda479dbf87cbdc
LeilaTI22 <l21590080@itsanjuan.edu.mx>
2024-09-19 21:42:42 -0600

    commit

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://root:ifwwpU00BqOQ1Cn281832ARqQ6OzR5yJ@dpg-crh5pld6l47c73c308a0-a.oregon-postgres.render.com/cetech_bads'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SCHEMA = 'cetech'
